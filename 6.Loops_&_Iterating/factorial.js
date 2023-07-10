0779e51b2b7e760f6504a99a0264ace98581c1b8
Ivan E Hernandez T <iamieht@gmail.com>
2023-07-10 21:35:51 +0200

    Loops - Ex2 - factorial

---
+++ 6.Loops_&_Iterating/factorial.js
@@ -0,0 +1,18 @@
+function factorial(integer) {
+  let result = 1;
+
+  for (let i = 1; i <= integer; i += 1) {
+    result *= i;
+  }
+
+  return result;
+}
+
+console.log(factorial(1)); // => 1
+console.log(factorial(2)); // => 2
+console.log(factorial(3)); // => 6
+console.log(factorial(4)); // => 24
+console.log(factorial(5)); // => 120
+console.log(factorial(6)); // => 720
+console.log(factorial(7)); // => 5040
+console.log(factorial(8)); // => 40320
