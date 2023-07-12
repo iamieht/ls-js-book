48c674e7d8ed5b7b597d2b9da77785ed70d3a4e3
Ivan E Hernandez T <iamieht@gmail.com>
2023-07-12 19:47:55 +0200

    Objects - Ex4

---
+++ 8.Objects/ex4.js
@@ -0,0 +1,11 @@
+let obj = {
+  b: 2,
+  a: 1,
+  c: 3,
+};
+
+let objKeys = Object.keys(obj);
+let upperKeys = objKeys.map((key) => key.toUpperCase());
+
+console.log(upperKeys);
+console.log(obj);
