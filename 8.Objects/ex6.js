18a3a2eb4186599bd9b85e21b4b0f4ae9f4c3459
Ivan E Hernandez T <iamieht@gmail.com>
2023-07-13 19:56:44 +0200

    Objects - Ex6

---
+++ 8.Objects/ex6.js
@@ -0,0 +1,8 @@
+// "foo" => primitive
+// 3.1415 => primitive
+// [ 'a', 'b', 'c' ] => Object
+// false => primitive
+// foo => Identifier
+// function bar() { return "bar"; } => Object
+// undefined => primitive
+// { a: 1, b: 2 } => Object
