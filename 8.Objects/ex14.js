8fb2bb80c70c1aa192ddf290f23eb4b26023c5bd
Ivan E Hernandez T <iamieht@gmail.com>
2023-07-14 18:38:19 +0200

    Objects - Ex14

---
+++ 8.Objects/ex14.js
@@ -0,0 +1,10 @@
+function hello(greeting, name) {
+  return greeting + " " + name;
+}
+
+function xyzzy() {
+  return { a: 1, b: 2, c: [3, 4, 5], d: {} };
+}
+
+const howdy = hello("Hi", "Grace");
+let foo = xyzzy();
